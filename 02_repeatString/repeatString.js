67a10e76051a32d2086adf0cec1baa78478a8471
TSN2222 <98364618+TSN2222@users.noreply.github.com>
2025-02-08 20:31:15 -0500

    Complete second module --02_repeatString--

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    if(num < 0) return 'ERROR';
 
+    let phrase = '';
+    for(i = 0; i < (num); i++) {
+        phrase += string;
+    }
+    return phrase;
 };
+
+repeatString('hey', 3);
 
 // Do not edit below this line
 module.exports = repeatString;
