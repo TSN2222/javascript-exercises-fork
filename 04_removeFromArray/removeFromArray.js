2d7ae0f4100b77e0b257781892076f163c1b643f
TSN2222 <98364618+TSN2222@users.noreply.github.com>
2025-02-10 21:28:39 -0500

    Complete fourth module --04_removeFromArray--

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,18 @@
-const removeFromArray = function() {
+//I cooked a little too hard here
+const removeFromArray = function(myArray, val0, val1, val2, val3) {
+    myArray.forEach((item, index) => {
+        if(item === val0){
+            delete myArray[index];
+        } else if(item === val1) {
+            delete myArray[index];
+        } else if (item === val2) {
+            delete myArray[index];
+        } else if (item === val3) {
+            delete myArray[index];
+        }
+    })
+    const cleanArray = myArray.filter(element => element !== null);
+    return cleanArray;
 };
 
 // Do not edit below this line
