d92f5345b020e47efd83fd1c34d02df40d24046a
TSN2222 <98364618+TSN2222@users.noreply.github.com>
2025-02-13 20:09:05 -0500

    Complete fifth module --05_sumAll--

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function(num, numTwo) {
 
+    if (!Number.isInteger(num) || !Number.isInteger(numTwo) || num < 0 || numTwo < 0) return 'ERROR';
+
+
+    if(num > numTwo) {
+        const temp = num;
+        num = numTwo;
+        numTwo = temp;
+    }
+
+    let sum = 0;
+    for(i = num; i <= numTwo; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
